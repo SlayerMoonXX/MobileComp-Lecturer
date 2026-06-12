085cc2fb5218391073b4274143668260d853b8a1
SlayerMoon <rafaelsitumorag8@gmail.com>
2026-06-12 08:35:40 +0700

    first commit

---
+++ lib/models/student.dart
@@ -0,0 +1,13 @@
+class Student {
+  final String name;
+  final String avatar;
+  final String domisili;
+  final String phone;
+
+  Student({
+    required this.name,
+    required this.avatar,
+    required this.domisili,
+    required this.phone,
+  });
+}
--- lib/session_7/constants/app_strings.dart
+++
@@ -1,5 +0,0 @@
-class AppStrings {
-  static const appName = 'My App';
-  static const profileTitle = 'Profile';
-  static const homeTitle = 'Home';
-}
